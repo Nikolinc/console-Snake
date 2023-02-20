92b184c52be51b49419bafb690af0ed42604f775
Nikolinc <tsigulskynikita@gmail.com>
2023-02-20 14:37:16 +0500

    playing field

--- ui.js
+++ ui.js
@@ -1,14 +1,26 @@
-'use strict';
-const React = require('react');
-const {Text} = require('ink');
+"use strict";
+const React = require("react");
+const { Text, Box, Color } = require("ink");
 const FIELD_SIZE = 16;
 const FIELD_ROW = [...new Array(FIELD_SIZE).keys()];
 
-
-const App = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
+const App = () => (
+	<Box flexDirection="column" alignItems="center">
+		<Text>
+			<Text color="green">Snake</Text> Game
+		</Text>
+		<Box flexDirection="column">
+			{FIELD_ROW.map((y) => (
+				<Box key={y}>
+					{FIELD_ROW.map((x) => (
+						<Box key={x}>
+							<Text> . </Text>
+						</Box>
+					))}
+				</Box>
+			))}
+		</Box>
+	</Box>
 );
 
 module.exports = App;
