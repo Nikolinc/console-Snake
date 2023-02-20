cd560cbb1d9bebffddccd79af41a65bcde21d290
Nikolinc <tsigulskynikita@gmail.com>
2023-02-20 18:37:10 +0500

    playing field

--- ui.js
+++ ui.js
@@ -1,26 +1,52 @@
 "use strict";
 const React = require("react");
-const { Text, Box, Color } = require("ink");
+const { useState } = require("react");
+const { Text, Box } = require("ink");
 const FIELD_SIZE = 16;
 const FIELD_ROW = [...new Array(FIELD_SIZE).keys()];
 
-const App = () => (
-	<Box flexDirection="column" alignItems="center">
-		<Text>
-			<Text color="green">Snake</Text> Game
-		</Text>
-		<Box flexDirection="column">
-			{FIELD_ROW.map((y) => (
-				<Box key={y}>
-					{FIELD_ROW.map((x) => (
-						<Box key={x}>
-							<Text> . </Text>
-						</Box>
-					))}
-				</Box>
-			))}
+let foodItem = {
+	x: Math.floor(Math.random() * FIELD_SIZE),
+	y: Math.floor(Math.random() * FIELD_SIZE),
+};
+
+function getItem(x, y, snakeSegments) {
+	if (foodItem.x === x && foodItem.y === y) {
+		return <Text color="red"> ♥ </Text>;
+	}
+
+	for (const segment of snakeSegments) {
+		if (segment.x === x && segment.y === y) {
+			return <Text color="green"> ■ </Text>;
+		}
+	}
+}
+
+const App = () => {
+	const [snakeSegments, setSnakeSegments] = useState([
+		{ x: 8, y: 8 },
+		{ x: 8, y: 7 },
+		{ x: 8, y: 6 },
+	]);
+
+	return (
+		<Box flexDirection="column" alignItems="center">
+			<Text>
+				<Text color="green">Snake</Text> Game
+			</Text>
+			<Box flexDirection="column">
+				{FIELD_ROW.map((y) => (
+					<Box key={y}>
+						{FIELD_ROW.map((x) => (
+							<Box key={x}>
+								<Text>{getItem(x, y, snakeSegments) || " . "}</Text>
+							</Box>
+						))}
+					</Box>
+				))}
+			</Box>
 		</Box>
-	</Box>
-);
+	);
+};
 
 module.exports = App;
